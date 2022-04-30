9b1a87591aea58b296fec721046e50e5e87873ea
fabiano <fv_souza@yahoo.com.br>
2022-04-30 18:36:12 -0300

    banco de dados

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,6 +1,7 @@
 from ast import arguments
 from turtle import home
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 hoteis = [
     {
         'hotel_id': 'apha',
@@ -28,51 +29,61 @@ hoteis = [
     },
 ]
 
+
+
 class Hoteis(Resource):
     def get(self):
-        return{'hoteis':hoteis}
+        return{'hoteis':[hotel.json() for hotel in HotelModel.query.all()]}
 
 class Hotel(Resource):
-    def find_hotel(hotel_id):
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return None
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome', type=str, required=True, help="O campo 'Nome' não pode ser em branco")
+    argumentos.add_argument('estrelas', type=str, required=True, help="O campo 'Nome' não pode ser em branco")
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+
 
     def get(self, hotel_id):
-        hotel = Hotel.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            return hotel
+            return hotel.json()
         return {'message': 'Hotel not found'}, 404  
 
-    def post(self, hotel_id): 
-        argumentos = reqparse.RequestParser()
-        argumentos.add_argument('nome')
-        argumentos.add_argument('estrelas')
-        argumentos.add_argument('diaria')
-        argumentos.add_argument('cidade')
-
-        dados = argumentos.parse_args()
-
-        novo_hotel = {
-            'hotel_id' : hotel_id,
-            'nome' : dados['nome'],
-            'estrelas': dados['estrelas'],
-            'diaria': dados['diaria'],
-            'cidade': dados['cidade']
-        }
-
-        hoteis.append(novo_hotel)
-        return novo_hotel, 200
-
-
-
-    def put(self, hotel_id): 
-        hotel = Hotel.find_hotel(hotel_id)
-        if hotel:
-            return hotel
+    def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return{"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400 
+       
+        dados = Hotel.argumentos.parse_args()
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return{'message': 'Aconteceu um erro interno na hora de salvar o Hotel'}, 500
+        return hotel.json()
         
 
+    def put(self, hotel_id): 
+        dados = Hotel.argumentos.parse_args()
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
+        if hotel_encontrado:
+            hotel_encontrado.update_hotel(**dados)
+            hotel_encontrado.salve_hotel()
+            return hotel_encontrado.json(), 200
 
-    def delete(self): 
-        pass
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return{'message': 'Aconteceu um erro interno na hora de salvar o Hotel'}, 500
+        return hotel.json(), 201
+
+        
+    def delete(self, hotel_id): 
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            try:
+                hotel.delete_hotel()
+            except:
+                return{'message':'Erro ao deletar o Hotel '}, 500
+            return {'message': 'Hotel deleted'}
+        return {'messege': 'Hotel not found.'}, 404
